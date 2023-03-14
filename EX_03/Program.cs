8b554fd1100c6d01194a62ac220687e393d6810f
DimaSannikov <dimasannikov@gmail.com>
2023-03-14 11:54:55 +0500

    added EX_03

---
+++ EX_03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное
+// число в двоичное.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+string RetBinaryNum(int number)
+{
+    string result = string.Empty;
+    while(number > 0)
+    {
+        result = Convert.ToString(number % 2) + result;
+        number = number / 2;
+        // Console.WriteLine(result);
+    }
+    return result;
+}
+Console.WriteLine(RetBinaryNum(num));
