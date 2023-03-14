a3b56b54c035a1f59c3d41857f3ab971a63e5369
DimaSannikov <dimasannikov@gmail.com>
2023-03-14 11:55:09 +0500

    added EX_05

---
+++ EX_05/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с
+// помощью поэлементного копирования.
+
+int[] array = {1, 2, 3, 4, 5, 6};
+
+int[] newArray(int[] array)
+{
+    int[] newArray = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+int[] secondNewArray(int[] array)
+{
+    int[] secondNewArray = new int[array.Length];
+    Array.Copy(array, secondNewArray, array.Length);
+    return secondNewArray;
+}
+
+Console.WriteLine("["+$"{String.Join(", ", newArray(array))}"+"]");
+Console.WriteLine();
+Console.WriteLine("["+$"{String.Join(", ", secondNewArray(array))}"+"]");
