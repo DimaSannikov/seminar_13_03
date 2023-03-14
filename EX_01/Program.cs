dd0cc03e17c5afe24bea1a3c0b746e3735a4be25
DimaSannikov <dimasannikov@gmail.com>
2023-03-14 11:54:39 +0500

    added EX_01

---
+++ EX_01/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (последний
+// элемент будет на первом месте, а первый - на последнем и т.д.)
+
+int[] array = new int[6] {1, 2, 3, 4, 5, 6};
+
+
+// Array.Reverse(array);
+// Console.WriteLine("["+$"{String.Join(", ", array)}"+"]");
+
+
+// int[] tempArray = new int[array.Length];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     tempArray[i] = array[(array.Length - 1) - i];
+// }
+
+// Array.Reverse(array);
+// Console.WriteLine("["+$"{String.Join(", ", tempArray)}"+"]");
+
+
+int[] tempArray = new int[array.Length];
+for (int i = 0; i < array.Length / 2; i++)
+{
+    int temp = array[i];
+    array[i] = array[(array.Length - 1) - i];
+    array[(array.Length - 1) - i] = temp;
+}
+
+Console.WriteLine("["+$"{String.Join(", ", array)}"+"]");
