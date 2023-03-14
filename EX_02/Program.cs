bf10f4ddf74a890d45a75b6cfe825a8fd6f32f57
DimaSannikov <dimasannikov@gmail.com>
2023-03-14 11:54:47 +0500

    added EX_02

---
+++ EX_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник с такими сторонами.
+
+Console.Write("Введите число 1: ");
+int side1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int side2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+int side3 = int.Parse(Console.ReadLine());
+
+if(side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 + side2)
+{
+    Console.WriteLine("Треугольник с такими сторонами может существовать");
+}
+else
+{
+    Console.WriteLine("Треугольник с такими сторонами не может существовать");
+}
