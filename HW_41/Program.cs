b78b02bd72c0c5f7d65bb75eb1f8d694918b3f5a
DimaSannikov <dimasannikov@gmail.com>
2023-03-14 11:55:31 +0500

    added HW_41

---
+++ HW_41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите количество чисел для расчета: ");
+int amount = int.Parse(Console.ReadLine());
+
+
+int ToCountNumbersMoreThanZero(int amount)
+{
+    int count = 0;
+    for (int i = 1; i <= amount; i++)
+    {
+        Console.Write($"Введите число {i}: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine($"Количество чисел больше 0 => {ToCountNumbersMoreThanZero(amount)}");
